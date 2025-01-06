330e7fd1177c83909cd37f2ccbaaf1a9e0c6c852
purna <abc@123.com>
2025-01-06 17:10:57 +0800

    conversation added

--- 07-array.sh
+++ 07-array.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 MOVIES=("jaffa" "kuffa" "affa")
-echo " First one is : ${MOVIES[0]}"
-echo " Second one is : ${MOVIES[1]}"
+echo "First one is : ${MOVIES[0]}"
+echo "Second one is : ${MOVIES[1]}"
 echo "Third one is : ${MOVIES[2]}"
 
 echo "All are is : ${MOVIES[@]}
