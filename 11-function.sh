e9cd92f65c62ce7843cd6c9d5161bce88ab05596
purna <abc@123.com>
2025-01-09 14:30:20 +0800

    install script

--- 11-function.sh
+++ 11-function.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 USRID=$( id -u)
-VALIDATE () {
-    if [$1 -ne 0]
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
     then
         echo "$2 ... FAILURE"
         exit 1
     else
-        echo " $2 ... SUCESSS"
+        echo "$2 ... SUCESSS"
     fi 
  }
 
-    if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ]
     then 
         echo "ERROR:: You must  have sudo access to execute this script"
         exit 1 #other than 1
@@ -19,19 +20,19 @@ VALIDATE () {
 
 dnf list installed mysql
 
-    if [ $? -ne 0 ] #not installed
-    then 
-        dnf install mysql -y
-        VALIDATE #? "Installing MYSQL"
-    else
-        echo " MysQL is already ... Installed"
-    fi
+if [ $? -ne 0 ] 
+then #not installed
+    dnf install mysql -y
+    VALIDATE #? "Installing MYSQL"
+else
+    echo "MysQL is already ... Installed"
+fi    
 
 dnf list installed git
-    if [ $? -ne 0 ] #not installed
-    then 
-        dnf install git -y
-        VALIDATE #? "Installing Git"
-    else
-        echo " Git is already ... Installed"
-    fi
+if [ $? -ne 0 ] #not installed
+then 
+    dnf install git -y
+    VALIDATE #? "Installing Git"
+else
+    echo " Git is already ... Installed"
+fi
