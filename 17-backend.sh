1db9a78872cfe3f5cfe8cf8392a646da07c9faef
purna <abc@123.com>
2025-01-09 17:11:13 +0800

    install script

--- 17-backend.sh
+++ 17-backend.sh
@@ -13,10 +13,10 @@ LOGS_FILE_NAME="$FOLDER_NAME/$LOG_FILE-$TIMESTAMP.log"
 VALIDATE (){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 ... $R FAILURE $N" &>>$LOGS_FILE_NAME
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
-        echo -e "$2 ... $G SUCESSS $N" &>>$LOGS_FILE_NAME
+        echo -e "$2 ... $G SUCESSS $N" 
     fi 
  }
 
