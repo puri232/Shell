5a1ed0ec64993e66a8da563e4109975251d7e61a
purna <abc@123.com>
2025-01-09 16:02:40 +0800

    install script

---
+++ 16-mysql.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USRID=$( id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+FOLDER_NAME="/var/log/expense.log"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOGS_FILE_NAME="$FOLDER_NAME/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N" &>>$LOGS_FILE_NAME
+        exit 1
+    else
+        echo -e "$2 ... $G SUCESSS $N" &>>$LOGS_FILE_NAME
+    fi 
+ }
+
+CHECK_ROOT() {
+if [ $USRID -ne 0 ]
+    then 
+        echo "ERROR:: You must  have sudo access to execute this script"
+        exit 1 #other than 1
+    fi
+}
+
+echo "script started executing at : $TIMESTAMP" &>>$LOGS_FILE_NAME
+
+CHECK_ROOT
+dnf install mysql-server -y &>>$LOGS_FILE_NAME
+VALIDATE $? "installing Mysql Server"
+
+systemctl enable mysqld &>>$LOGS_FILE_NAME
+VALIDATE $? "Enableing mysql server"
+
+systemctl start mysqld &>>$LOGS_FILE_NAME
+VALIDATE $? "starting mysql server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGS_FILE_NAME
+VALIDATE $? "Setting root pasword"
