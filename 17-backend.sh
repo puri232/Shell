c3dc7c0a926ba4c4709d9d577475957294bece45
purna <abc@123.com>
2025-01-10 16:15:53 +0800

    adding

--- 17-backend.sh
+++ 17-backend.sh
@@ -82,5 +82,5 @@ VALIDATE $? "Starting Backend"
 dnf install mysql -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h mysql.daws82s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+mysql -h mysql.purnachandra.space -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
 VALIDATE $? "Setting up the transactions schema and tables"
