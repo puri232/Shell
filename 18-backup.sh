9abe46e52e5e9c579306c0b595fa3f609b7b2424
purna <abc@123.com>
2025-01-14 16:32:24 +0800

    abcdf mi thatha gudala duffu

--- 18-backup.sh
+++ 18-backup.sh
@@ -5,19 +5,19 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-SDIR=$1
-DDIR=$2
-DAYS=${3:-14}
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # if user is not providing number of days, we are taking 14 as default
 
-FOLDER_NAME="/home/ec2-user/shellscript.logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1 )
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOGS_FILE_NAME="$FOLDER_NAME/$LOG_FILE-$TIMESTAMP.log"
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 USAGE(){
+    #echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
     echo -e "$R USAGE:: $N backup <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
     exit 1
 }
 
-mkdir -p /home/ec2-user/shellscript.logs
-echo "script started executing at : $TIMESTAMP" &>>$LOGS_FILE_NAME
+mkdir -p /home/ec2-user/shellscript-logs
