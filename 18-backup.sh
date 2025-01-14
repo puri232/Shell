d980355ef65e3e7884f8d9262955af5997cf5136
purna <abc@123.com>
2025-01-14 16:48:32 +0800

    abcdf mi thatha gudala duffu

--- 18-backup.sh
+++ 18-backup.sh
@@ -41,7 +41,7 @@ fi
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-FILES=$( find $SOURCE_DIR -name "*.log" +mtime +$DAYS )
+FILES=$( find $SOURCE_DIR -name "*.log" -mtime +$DAYS )
 
 if [ -n $FILES ]
 then
